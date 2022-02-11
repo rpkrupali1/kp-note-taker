eb6973ee164595b6025e3d9062bfcb86f758327f
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-11 05:31:52 -0600

    added get call to filter by specific title

--- server.js
+++ server.js
@@ -13,11 +13,24 @@ function filterbyQuery(query,notesArray){
     return filterResults;
 }
 
+function filterByTitle(title,notesArray){
+    const result = notesArray.filter(note => note.title === title)[0];
+    return result;
+}
+
 app.get('/api/notes',(req,res) => {
     let results = notes;
     if(req.query)
         results = filterbyQuery(req.query,results);
     res.json(results);
+});
+
+app.get('/api/notes/:title',(req,res) => {
+    const result = filterByTitle(req.params.title,notes);
+    if(result)
+        res.json(result);
+    else
+        res.send(404);
 });
 
 app.listen(PORT, () => {
