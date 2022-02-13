41cdc6e8a93d5dc991f29eb2776c2c8aa7355066
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-12 18:42:47 -0600

    updates to readme

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# Note Taker Starter Code
+# Note Taker 
+
+  Generates team profile data based on user input
+
+   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)  [![License: Apache](https://img.shields.io/badge/License-Apache-yellow.svg)](https://opensource.org/licenses/Apache) 
+
+  # Table of contents
+  * [Purpose](#purpose)
+  * [Usage](#usage)
+  * [Technologies Used](#technologies-used)
+  * [Test Instructions](#test-instructions)
+  * [More Information](#more-information)
+  * [Contributors](#contributors)
+  * [License](#license)
+
+  ## Purpose
+  Application that can be used to write and save notes.
+
+  ## Usage
+  Please refer below link to use this application:
+  https://note-taker-kp-challenge.herokuapp.com/
+
+  ## Technologies Used
+  - HTML
+  - CSS
+  - Java Script
+  - NodeJs
+  - Express JS
+  
+  ## Test Instructions
+ npm run test
+ ![Test](./images/Testruns.PNG)
+
+  ## More Information
+  Please find below acceptance criteria of project:
+
+    GIVEN a note-taking application
+    WHEN I open the Note Taker
+    THEN I am presented with a landing page with a link to a notes page
+    WHEN I click on the link to the notes page
+    THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+    WHEN I enter a new note title and the note’s text
+    THEN a Save icon appears in the navigation at the top of the page
+    WHEN I click on the Save icon
+    THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+    WHEN I click on an existing note in the list in the left-hand column
+    THEN that note appears in the right-hand column
+    WHEN I click on the Write icon in the navigation at the top of the page
+    THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
+  ## Deployed application:
+    ![DeployedApplication](./images/DeployedApplication.PNG)
+
+  ## Contributors
+  Krupali
+  
+  ## License
+  Please consult the attached LICENSE file for details. All rights not explicitly granted by the MIT,Apache License are reserved by Original Author.    
+
+  Copyright (c) 2021
---
+++ __tests__/notes.test.js
@@ -0,0 +1,151 @@
+const {
+    filterbyQuery,
+    filterById,
+    filterByTitle,
+    createNewNote,
+    validateNote,
+    deleteNote,
+    validateNoteId
+} = require('../lib/notes');
+const notes = require('../db/db.json');
+const fs = require('fs');
+jest.mock('fs');
+
+test('test for filterByQuery', () => {
+    const notes = [
+        {
+            id: 1,
+            title: 'TestNote1',
+            text: 'Note1 created for unit test purpose'
+        },
+        {
+            id: 2,
+            title: 'TestNote2',
+            text: 'Note2 created for unit test purpose'
+        },
+        {
+            id: 3,
+            title: 'TestNote3',
+            text: 'Note3 created for unit test purpose'
+        }
+    ];
+    const actualNote = filterbyQuery({title:'TestNote2'},notes);
+    expect(actualNote.length).toEqual(1);
+});
+
+test('tests for filterbyId',() => {
+    const notes = [
+        {
+            id: '1',
+            title: 'TestNote1',
+            text: 'Note1 created for unit test purpose'
+        },
+        {
+            id: '2',
+            title: 'TestNote2',
+            text: 'Note2 created for unit test purpose'
+        },
+        {
+            id: '3',
+            title: 'TestNote3',
+            text: 'Note3 created for unit test purpose'
+        }
+    ];
+
+    const filteredNote = filterById('2',notes);
+    expect(filteredNote.title).toBe('TestNote2');
+});
+
+test('tests for filterbyTitle',() => {
+    const notes = [
+        {
+            id: '1',
+            title: 'TestNote1',
+            text: 'Note1 created for unit test purpose'
+        },
+        {
+            id: '2',
+            title: 'TestNote2',
+            text: 'Note2 created for unit test purpose'
+        },
+        {
+            id: '3',
+            title: 'TestNote3',
+            text: 'Note3 created for unit test purpose'
+        }
+    ];
+
+    const filteredNote = filterByTitle('TestNote2',notes);
+    expect(filteredNote.id).toBe('2');
+});
+
+test('create new note', () => {
+    const note = createNewNote({title:'create object test',text:'this note is created from tests'},notes);
+    expect(note.title).toBe('create object test');
+    expect(note.text).toBe('this note is created from tests');
+});
+
+test('test validateNote method', () => {
+    const validTest = validateNote({text:'test',title: 'test'});
+    const invalidTest1 = validateNote({title: 'test'});
+    const invalidTest2 = validateNote({text:'test'});
+    const invalidTest3 = validateNote({text:1,title: 'test'});
+    expect(validTest).toBe(true);
+    expect(invalidTest1).toBe(false);
+    expect(invalidTest2).toBe(false);
+    expect(invalidTest3).toBe(false);
+});
+
+test('test validateNote method', () => {
+    const notes = [
+        {
+            id: 1,
+            title: 'TestNote1',
+            text: 'Note1 created for unit test purpose'
+        },
+        {
+            id: 2,
+            title: 'TestNote2',
+            text: 'Note2 created for unit test purpose'
+        },
+        {
+            id: 3,
+            title: 'TestNote3',
+            text: 'Note3 created for unit test purpose'
+        }
+    ];
+    const actualResult = validateNote()
+});
+
+test('test delete object', () => {
+    const notes = [
+        {
+            id: '1',
+            title: 'TestNote1',
+            text: 'Note1 created for unit test purpose'
+        },
+        {
+            id: '2',
+            title: 'TestNote2',
+            text: 'Note2 created for unit test purpose'
+        },
+        {
+            id: '3',
+            title: 'TestNote3',
+            text: 'Note3 created for unit test purpose'
+        }
+    ];
+
+    deleteNote('3',notes);
+    expect(notes).toMatchObject([
+        {
+            id: '1',
+            title: 'TestNote1',
+            text: 'Note1 created for unit test purpose'
+        },
+        {
+            id: '2',
+            title: 'TestNote2',
+            text: 'Note2 created for unit test purpose'
+        }]);
+});
